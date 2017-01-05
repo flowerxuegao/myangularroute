879ce95c1278330d2b0c0c3779d72b3d50449174
flowerxuegao <1057796814@qq.com>
2017-01-05 23:52:26 +0800

    my angularroute

---
+++ text/controllers.js
@@ -0,0 +1,8 @@
+angular.module("ctrls",["services"])
+.controller("index",["$scope","$http","indexDate",function($scope,$http,indexDate){
+    indexDate.then(function(data){
+        $scope.data = data.data;
+    })
+}]).controller("list",function(){
+
+})
